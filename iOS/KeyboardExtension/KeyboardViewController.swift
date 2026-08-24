a0f7db974f61483473ab435be51f4a4416f8d21a
wang1ang <yang.wang@smartnews.com>
2026-08-25 08:25:28 +0900

    iOS: test negative candidate top inset

--- iOS/KeyboardExtension/KeyboardViewController.swift
+++ iOS/KeyboardExtension/KeyboardViewController.swift
@@ -52,7 +52,7 @@ final class KeyboardViewController: UIInputViewController {
         NSLayoutConstraint.activate([
             root.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5),
             root.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -5),
-            root.topAnchor.constraint(equalTo: view.topAnchor),
+            root.topAnchor.constraint(equalTo: view.topAnchor, constant: -6),
             root.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -6)
         ])
 
